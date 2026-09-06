4aa35ed4ffda94aa8e36c4ceb8568c5782729c21
Gunnar Beutner <gunnar@beutner.de>
2026-09-06 20:03:49 +0200

    Add the -NOBRIEFING startup switch

--- code/init.cpp
+++ code/init.cpp
@@ -1697,6 +1697,11 @@ bool Parse_Command_Line(int argc, char * argv[])
 			continue;
 		}
 
+		if (stricmp(string, "-NOBRIEFING") == 0) {
+			Debug_Skip_Briefing = true;
+			continue;
+		}
+
 		if (strstr(string, "-MPDEBUG")) {
 			Session.ShowInternetDebug = true;
 		}
