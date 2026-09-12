f95e4c6cc4a2513440a2573ed8e7bcd43bc14a5f
Gunnar Beutner <gunnar@beutner.de>
2026-09-12 07:05:45 +0200

    Read the millisecond clock through std::chrono

--- code/mstimer.cpp
+++ code/mstimer.cpp
@@ -13,7 +13,10 @@
 
 #include "win.h"
 
+#include <chrono>
 
+
+#ifdef _WIN32
 // Windows 10 SDKs such as 10.0.19041 lack this Windows 11 flag.
 #ifndef PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION
 #define PROCESS_POWER_THROTTLING_IGNORE_TIMER_RESOLUTION 0x4
@@ -35,26 +38,37 @@ static struct MillisecondResolutionClass
 	}
 	~MillisecondResolutionClass(void) { timeEndPeriod(1); }
 } MillisecondResolution;
+#endif
 
 
 /// <summary>
-/// Fetches the current millisecond reading of the system clock.
-/// This is the sampling routine that the timer templates call whenever they need to
-/// know how much time has passed.
+/// Returns the milliseconds elapsed since the first call. The origin is this process, not
+/// the machine, so readings are only meaningful against one another.
 /// </summary>
-/// <returns>Returns with the number of milliseconds elapsed since Windows started.</returns>
-int MillisecondSystemTimerClass::operator () (void) const
+unsigned int System_Milliseconds(void)
 {
-	return(timeGetTime());
+	using namespace std::chrono;
+
+	static steady_clock::time_point const started = steady_clock::now();
+	return((unsigned int)duration_cast<milliseconds>(steady_clock::now() - started).count());
 }
 
 
 /// <summary>
-/// Converts the timer into its current millisecond reading.
-/// This routine lets the timer object be used wherever a plain time value is expected.
+/// Fetches the current millisecond reading. This is the sampling routine that the timer
+/// templates call whenever they need to know how much time has passed.
 /// </summary>
-/// <returns>Returns with the number of milliseconds elapsed since Windows started.</returns>
+int MillisecondSystemTimerClass::operator () (void) const
+{
+	return((int)System_Milliseconds());
+}
+
+
+/// <summary>
+/// Converts the timer into its current millisecond reading, so that it can be used wherever
+/// a plain time value is expected.
+/// </summary>
 MillisecondSystemTimerClass::operator int (void) const
 {
-	return(timeGetTime());
+	return((int)System_Milliseconds());
 }
