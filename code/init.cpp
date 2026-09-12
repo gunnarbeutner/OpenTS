6f834c836ed61e9d3992c160efed03ff688fa3b2
Gunnar Beutner <gunnar@beutner.de>
2026-09-12 08:49:31 +0200

    Play MP4 movies through the browser media element

--- code/init.cpp
+++ code/init.cpp
@@ -2452,6 +2452,11 @@ static bool Init_Secondary_Mixfiles(void)
 	}
 
 	if (MoviesMix == NULL) {
+#if defined(OPENTS_MP4_MOVIES)
+		if (Movie_Is_Available("WWLOGO")) {
+			return(true);
+		}
+#endif
 		return(false);
 	}
 
